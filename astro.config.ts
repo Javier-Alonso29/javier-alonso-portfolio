fa9908ea293417a85daf37077b73fb22f88925c5
Javier-Alonso29 <imjavieralonso@gmail.com>
2026-02-17 23:48:12 -0600

    feat: add sitemap integration and structured data for SEO improvements

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,17 @@ import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tehctim42.com',
+  site: 'https://javieralonso.netlify.app',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
     mdx(),
+    sitemap(),
   ],
 });
